b00140678acf66ff74abbb18e5efcb9d874e9044
Rodriguez Alexis <alexisrodriguezbepy@gmail.com>
2023-03-02 20:12:27 -0300

    1 entrega

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+import NavBar from './components/NavBar/NavBar';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+function App() {
+  
+
+  return (
+    <>
+    <NavBar />
+    <ItemListContainer greeting="Bienvenidos a Tu Tienda"/> 
+    
+    </>
+    
+
+
+  )
+}
+
+export default App
---
+++ src/components/CardWidget/CardWidget.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CardWidget = () => {
+  return (
+    <div> 0 🛒</div>
+  )
+}
+
+export default CardWidget
